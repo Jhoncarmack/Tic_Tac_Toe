ad9a3186126c457e5b74a34be52984b2fc969834
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2026-09-24 14:09:40 +0900

    feat: IIFE로 팩토리 함수 묶고 콘솔에 문자열 표시
     - 콘솔에 사용자를 표시하기위해 문자열을 담는 변수를 2개로 분리
     - player함수로 사용자 이름과 마커를 인수로 전달하면 문자열로 표시

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-// HTML, CSS, JavaScript 파일을 만들어 프로젝트를 준비하고, Git 저장소 설정도 마치세요.
-
-// Gameboard 객체 안에 게임판을 배열로 저장할 것이니, 여기서부터 시작하세요! 플레이어도 객체로 저장할 것이며,
-// 게임 자체의 흐름을 제어하는 객체도 필요할 것입니다.
-
-function newGameBoard() {
-   const gameBoard = [];
-   for (let i = 0; i < 3; i++) {
-      const row = [];
-      for (let j = 0; j < 3; j++) {
-         row.push(0);
+const createGameBoard = (() => {
+   let gamePlayer;
+   let gamePlayer1 = "";
+   let gamePlayer2 = "";
+   const newGameBoard = () => {
+      const gameBoard = [];
+      for (let i = 0; i < 3; i++) {
+         const row = [];
+         for (let j = 0; j < 3; j++) {
+            row.push(0);
+         }
+         gameBoard.push(row);
       }
-      gameBoard.push(row);
-   }
-   console.log(gameBoard);
-}
+      console.log(gameBoard);
+   };
+   const player = (name, marker) => {
+      gamePlayer = name + marker;
+      if (gamePlayer1 === "") {
+         gamePlayer1 = gamePlayer;
+         console.log(gamePlayer1);
+      } else {
+         gamePlayer2 = gamePlayer;
+         console.log(gamePlayer2);
+      }
+   };
+   return { newGameBoard, player };
+})();
+createGameBoard.newGameBoard();
+createGameBoard.player("hyeon", "X");
+createGameBoard.player("hyeon", "O");
 
-newGameBoard();
+//  팩토리함수로 플레이어가 어떻게 할수있을까
+//  플레이어가 객체로 저장하는데 어떻게 내는 걸 맞추고 게임을 진행시킬수있을까
+//  게임의 흐름을 제어하는 객체도 만들자
+// 각함수끼리 값을 던져서 그함수가 실행되게 하는게 이번 과제 키포인트같음
+// 하나의 팩토리함수에서 사용되게끔하고
